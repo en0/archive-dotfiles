e412b284c46c6b635f3fe0a8d83c301e07232d56
Ian Laird <ilaird@unblinkinginfomatics.com>
2015-04-28 13:34:07 -0600

    Added examples

--- .scripts/git_prompt.sh
+++ .scripts/git_prompt.sh
@@ -75,9 +75,10 @@ function set_prompt_header() {
     echo -e "${branch}${staged}${unstaged}${prodj_root}"
 }
 
+# Examples: 
 # With a prompt and a header
 #export PS1='\[\e[1;36m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\033[m\]'
 #export PROMPT_COMMAND=set_prompt_header
 
 # With a fancey prompt
-export PROMPT_COMMAND=set_ps1
+#export PROMPT_COMMAND=set_ps1
